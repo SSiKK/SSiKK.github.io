25f00b1bd93f4f26cb43101321879fd3307be3d6
Kyle Almryde <kyle.almryde@gmail.com>
2016-04-10 16:53:20 -0500

    moved application logic to own js file (main.js), tidied up code to make a little more readable

--- js/src/camera.js
+++ js/src/camera.js
@@ -1,5 +1,5 @@
 var YDYW_Camera = SVG_Imitator.extend({
-	init: function (canvas) { // Initialize 
+	init: function (canvas) { // Initialize
 		//Attributes
 		this.left = null;
 		this.top = null;
@@ -9,9 +9,13 @@ var YDYW_Camera = SVG_Imitator.extend({
 		// Canvas on which the object is created.
 		this.canvas = null;
 
+		this.viewport = null; // The viewing window, should be a rect
+
+
 		//Feature specific status flags
 		this.on = false;
 		this.fullScreenMode = false;
+
 
 		if (canvas!==undefined && canvas!== null) {
 			this.attachToCanvas(canvas);
@@ -20,10 +24,16 @@ var YDYW_Camera = SVG_Imitator.extend({
 	attachToCanvas: function(canvas) {
 		this.canvas = canvas;
 	},
-	
+
 	draw: function () {
 		// Draw
 		console.log ("being drawn!", this);
+		this.viewport = new fabric.Rect({
+			originX: 'center',
+			originY: 'center',
+			fill: 'rgb(255,255,255,1.0)',
+
+		});
 	}
 
 });
